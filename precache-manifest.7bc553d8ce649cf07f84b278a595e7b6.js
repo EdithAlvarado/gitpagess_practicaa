e512aea475ea67713f186ba7df0b14a76d53d73c
edithalvarado <alvaradogutierrezedithalejandr@gmail.com>
2025-06-26 12:39:12 -0600

    Updates

---
+++ precache-manifest.7bc553d8ce649cf07f84b278a595e7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa990d91158777f3ddf9a532e11be88",
+    "url": "EdithAlvarado.github.io/gitpagess_practicaa/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "EdithAlvarado.github.io/gitpagess_practicaa/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "b0069fbee5ea61773e10",
+    "url": "EdithAlvarado.github.io/gitpagess_practicaa/static/css/main.37131298.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "EdithAlvarado.github.io/gitpagess_practicaa/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "b0069fbee5ea61773e10",
+    "url": "EdithAlvarado.github.io/gitpagess_practicaa/static/js/main.18ab947a.chunk.js"
+  },
+  {
+    "revision": "91f42cafc4de0ecf17f6",
+    "url": "EdithAlvarado.github.io/gitpagess_practicaa/static/js/runtime~main.5c949290.js"
+  }
+]);
